a3bb91e3e180edc6fbcee9b70e1b66b2d778ea9e
Stefan Lehmann <bernd49634@web.de>
2022-01-24 18:13:23 +0100

    wow

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import './App.css';
 
 function App() {
 
-  /*const serverUrl = process.env.REACT_APP_YOUR_SERVER_URL;
-const appId = process.env.REACT_APP_YOUR_APP_ID;
+ Moralis.serverUrl = "https://t4sirwflpnha.usemoralis.com:2053/server";
+Moralis.appId = "2VLj5S3kq8IKfTV2jEkJME6MYTLJniAdydB1xBnk";
 Moralis.start({ serverUrl, appId });
 
 /** Add from here down */
-/*async function login() {
-  let user = Moralis.User.current();
+async function login() {
+  let user = await Moralis.User.current();
   if (!user) {
    try {
       user = await Moralis.authenticate({ signingMessage: "Hello World!" })
@@ -26,11 +26,7 @@ async function logOut() {
   console.log("start of logging out")
   await Moralis.User.logOut();
   console.log("logged out");
-}*/
-
-//<button id="btn-login" onClick={login}>Moralis Login</button>
-//<button id="btn-logout" onClick={logOut}>Logout </button>
-        
+}
 
 
   return (
@@ -39,7 +35,8 @@ async function logOut() {
 
       
       Hello
-      
+      <button id="btn-login" onClick={login}>Moralis Login</button>
+    <button id="btn-logout" onClick={logOut}>Logout </button>
       </header>
     </div>
   );
