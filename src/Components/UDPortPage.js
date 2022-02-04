17f59e40eb6f80f37509d7a024ed4daefdf127bd
Stefan Lehmann <bernd49634@web.de>
2022-02-04 18:45:41 +0100

    fixed PortPage not showing ERC's when you only own 1

--- src/Components/UDPortPage.js
+++ src/Components/UDPortPage.js
@@ -461,7 +461,7 @@ const UDPortPage = (props) => {
     return <Navigate to={redirect} />
   }
 
-  if(areTokensFetched == true && finalObject.length <= 1){
+  if(areTokensFetched == true && finalObject.length < 1){
     return(<div className="pages " style={{height: "100vh"}}>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
 
